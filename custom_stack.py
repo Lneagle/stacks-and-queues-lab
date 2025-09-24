5dbb8498cb4f02637bd29d2d9573e19d3ed5b32b
Lacey Murphy <lneagle@gmail.com>
2025-09-24 18:22:47 -0400

    Implemented stack and queue

--- custom_stack.py
+++ custom_stack.py
@@ -1,7 +1,48 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+        print(f'Pushed {item} to stack: {self.items}')
+
+    def pop(self):
+        if not self.is_empty():
+            removed = self.items.pop()
+            print(f'Popped {removed} from stack: {self.items}')
+            return removed
+        else:
+            print('Stack is empty. Nothing to pop.')
+            return None
+        
+    def peek(self):
+        if not self.is_empty():
+            print(f'Top of stack is: {self.items[-1]}')
+            return self.items[-1]
+        else:
+            print('Stack is empty. No top element.')
+            return None
+        
+    def is_empty(self):
+        return len(self.items) == 0
+
 def is_valid_parentheses(s: str) -> bool:
     """
     Return True if the string contains valid, balanced parentheses.
     Only (), {}, and [] are considered valid.
     """
     # TODO: Implement stack logic to validate parentheses
-    pass
+    tester = Stack()
+    for c in s:
+        if c in ['{', '[', '(']:
+            tester.push(c)
+        elif c == '}':
+            if tester.peek() == '{':
+                tester.pop()
+        elif c == ']':
+            if tester.peek() == '[':
+                tester.pop()
+        elif c == ')':
+            if tester.peek() == '(':
+                tester.pop()
+    return tester.is_empty()
